4ced63946261fe96c36b06815b078a4ce113a92d
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-05-28 22:33:31 +0200

    first commit

---
+++ GamerPilotLanding/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GamerPilotLanding.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GamerPilotLanding/gulpfile.js
@@ -0,0 +1,16 @@
+﻿var gulp = require("gulp"),
+    fs = require("fs"),
+    sass = require("gulp-sass");
+
+// other content removed
+
+gulp.task("sass", function () {
+    return gulp.src('Styles/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('Styles/*.scss', ['sass']);
+    // Other watchers
+})
